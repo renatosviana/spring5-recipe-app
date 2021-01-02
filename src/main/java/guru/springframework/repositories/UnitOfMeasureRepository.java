dc39087074faf8a8d76d3454027a742d734da2dd
RenatoDaSilvaViana <renatosviana@gmail.com>
2021-01-02 17:36:49 -0500

    Added Optional method to CrudRepository.

--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -4,5 +4,8 @@ import guru.springframework.domain.Category;
 import guru.springframework.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
